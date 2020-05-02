a5ea1a7e658d23c378ab3b1e743d5a325662b080
Nxxthing <62184658+Nxxthing@users.noreply.github.com>
2020-05-02 12:17:29 +0200

    [fig 1] added text
    Added text to [fig 1], only button missing

--- Noughts x Crosses [fig 1].py
+++ Noughts x Crosses [fig 1].py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.font as tkFont
+
 root=tk.Tk()
 c=tk.Canvas(root, bg="#123f56", height=600, width=1200)
 c.pack(fill=tk.BOTH, expand=True)
@@ -40,3 +42,14 @@ c.create_arc(480,290,520,330,start=45,extent=180,outline="#eb9d14",width=5.0,sty
 c.create_arc(680,490,720,530,start=225,extent=180,outline="#eb9d14",width=5.0,style=tk.ARC)
 c.create_line(485,322.5,687.5,525,fill="#eb9d14",width=5.0)
 c.create_line(512.5,295,715,497.5,fill="#eb9d14",width=5.0)
+
+# Name text
+font = tkFont.Font(family='ModernDeco', size=65)
+font2 = tkFont.Font(family='ModernDeco', size=30)
+font3 = tkFont.Font(family='ModernDeco', size=35)
+photo = tk.PhotoImage(file="N x C rainbow.gif")
+c.create_image(600,105,image= photo)
+#c.create_text(600,105,text="NOUGHTS X CROSSES",font=font, fill="white")
+c.create_text(600,170,text="by Nxxthing",font=font2, fill="white")
+c.create_text(250,425,text="Be the first to align 3 symbols!",font=font3, fill="white",width=200)
+
