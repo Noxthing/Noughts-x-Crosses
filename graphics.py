b1070f7ca4f1a8d0037df015dc92fa3c9a12a4a5
Nxxthing <62184658+Nxxthing@users.noreply.github.com>
2020-06-14 17:46:15 +0200

    Added Victory Lines
    Victory lines are now available in graphics module, use 1, 2 or 3 for vertical or horizontal lines and True or False for diagonal lines.

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-def calc_coords(y, sx, sy):
+def calc_box_coords(y, sx, sy):
     x1 = sx*100+370
     x2 = x1+60
     y1 = sy*100+180+y
@@ -11,11 +11,11 @@ Draws a nought
 c: a Canvas object
 sx: 
     """
-    x1, y1, x2, y2 = calc_coords(y, sx, sy)
+    x1, y1, x2, y2 = calc_box_coords(y, sx, sy)
     c.create_oval(x1,y1,x2,y2,width=5.0, outline="#f26a5a")
 
 def draw_cross(c, y, sx, sy):
-    x1, y1, x2, y2 = calc_coords(y, sx, sy)
+    x1, y1, x2, y2 = calc_box_coords(y, sx, sy)
     c.create_line(x1,y1,x2,y2,width=5.0, fill="#1290af")
     c.create_line(x2,y1,x1,y2,width=5.0, fill="#1290af")
 
@@ -26,3 +26,20 @@ def draw_grid(c,y):
     c.create_line(650,y,650,y+300,width=5.0, fill="white")
     c.create_line(450,y+100,750,y+100,width=5.0, fill="white")
     c.create_line(450,y+200,750,y+200,width=5.0, fill="white")
+
+def draw_victory_line_h(c, sy):
+    c.create_line(450,(sy+1)*100,750,(sy+1)*100,width=10.0, fill="#eb9d14")
+
+def draw_victory_line_v(c, sx):
+    c.create_line((sx+4)*100,150,(sx+4)*100,450,width=10.0, fill="#eb9d14")
+
+def draw_victory_line_d1(c):
+    c.create_line(450,150,750,450,width=10.0, fill="#eb9d14")
+
+def draw_victory_line_d2(c):
+    c.create_line(750,150,450,450,width=10.0, fill="#eb9d14")
+
+def draw_victory_line_d(c,is_l):
+    x1, x2 = (450, 750) if is_l else (750, 450)
+    y1, y2 = 150, 450
+    c.create_line(x1,y1,x2,y2,width=10.0, fill="#eb9d14")
